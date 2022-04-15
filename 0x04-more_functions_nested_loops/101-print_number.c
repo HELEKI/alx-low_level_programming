0ef83c8b3e122e11e072a5566c035fb809d85a8d
HELEKI <helekiobura@gmail.com>
2022-04-14 17:41:54 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,43 +3,45 @@
 
 
 /**
- * print_number - function that prints an integer..
- * @n: input value to check
- * Return: nothing.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
+
 void print_number(int n)
 {
-	unsigned int num, num2;
-	int i;
-	int aux = 1;
+	unsigned int m, d, count;
+
 
 	if (n < 0)
+
 	{
-		n = n * -1;
-		_putchar('-');
+		_putchar(45);
+		m = n * -1;
 
 	}
-	num = n;
-	num2 = num;
-
-	if (num > 9)
-	{
-		while (num >= 10)
-
-		{
-			aux = aux * 10;
-			num = num / 10;
-
-		}
-		_putchar((num2 / aux) + '0');
-		aux = aux / 10;
-
-		for (i = aux; i >= 1; i = i / 10)
-
-			_putchar((num2 / i) % 10 + '0');
-	}
-
 	else
+	{
+		m = n;
+	}
 
-		_putchar(num + '0');
+	d = m;
+
+	count = 1;
+
+	while (d > 9)
+
+	{
+		d /= 10;
+
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+
+	{
+
+		_putchar(((m / count) % 10) + 48);
+
+	}
 }
