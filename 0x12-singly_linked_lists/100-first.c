61673e78eb09157e04b669a19d88e588b2c92878
HELEKI <helekiobura@gmail.com>
2022-05-24 08:08:50 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void print_something(void) __attribute__ ((constructor));
 
 /**
- *print_something - prints a msg before the main
+ * print_something - prints a msg before the main
  */
 
 void print_something(void)
@@ -11,5 +11,5 @@ void print_something(void)
 
 	printf("Sanity with alittle arrogance,\n"
 
-	       "Self love!\n");
+	       "Ni kujilombotove tu!\n");
 }
