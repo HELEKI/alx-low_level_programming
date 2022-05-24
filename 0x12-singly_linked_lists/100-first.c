27e632c73cff528df55f61f368fd6d2771d7085b
HELEKI <helekiobura@gmail.com>
2022-05-24 08:02:46 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_something(void) __attribute__ ((constructor));
+
+/**
+ *print_something - prints a msg before the main
+ */
+
+void print_something(void)
+{
+
+	printf("Sanity with alittle arrogance,\n"
+
+	       "Ni kujilombotove tu huku!\n");
+}
