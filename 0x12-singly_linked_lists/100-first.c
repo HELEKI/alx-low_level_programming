53b8b857816b3733ca49b31f4776eabda8738848
HELEKI <helekiobura@gmail.com>
2022-05-24 08:16:49 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,12 @@
 void print_something(void) __attribute__ ((constructor));
 
 /**
- * print_something - prints a msg before the main
+ *print_something - prints a msg before the main
  */
 
 void print_something(void)
 {
+	printf("Hey! You see, you ought to know,\n"
 
-	printf("Sanity with alittle arrogance,\n"
-
-	       "Ni kujilombotove tu!\n");
+	       "I am Heliakim!\n");
 }
